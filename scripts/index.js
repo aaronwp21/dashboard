73ad92fc4621aecc85cadf18f078f6bb463901a4
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-08-20 17:54:58 +0100

    joke api update

--- scripts/index.js
+++ scripts/index.js
@@ -19,21 +19,20 @@ const displayDate = () => {
 const dateContainer = document.getElementById('date-wrapper');
 dateContainer.append(displayDate());
 
+const weatherContainer = document.getElementById('weather-wrapper');
 
-const weatherContainer = document.getElementById("weather-wrapper");
-
-function showSpinner(area){
+function showSpinner(area) {
   area.innerHTML = `<div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
   <span class="visually-hidden">Loading...</span>
 </div>`;
 }
 
-function showError(area){
+function showError(area) {
   area.innerHTML = `<p class="section-info">Sorry this cannot be loaded</p>`;
 }
 
 function renderWeather(data) {
-      weatherContainer.innerHTML = `<div id="location-container" class="location-container">
+  weatherContainer.innerHTML = `<div id="location-container" class="location-container">
         <p class="section-titles">Location</p>
       <p class="section-info">${data.location.name}</p></div>
       <div class="current-container">
@@ -49,7 +48,7 @@ function renderWeather(data) {
           <div id="sunset" class="sunrise-sunset">
           <img src="/assets/weather/64x64/night/113.png" alt="Sunset"><p class="section-info">${data.forecast.forecastday[0].astro.sunset}</p></div>
         </div>
-      </div>`
+      </div>`;
 }
 
 async function getWeather() {
@@ -63,7 +62,9 @@ async function getWeather() {
       });
     });
     const { latitude, longitude } = position.coords;
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=d164cbfef0cd4d0b9e7200203230302&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`);
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=d164cbfef0cd4d0b9e7200203230302&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`,
+    );
 
     if (!response.ok) {
       throw response;
@@ -78,80 +79,69 @@ async function getWeather() {
 
 getWeather();
 
-const jokeWrapper = document.getElementById("joke-wrapper");
+const jokeWrapper = document.getElementById('joke-wrapper');
 
-function renderJoke(data){
-  const randomNum = Math.floor(Math.random() * data.length);
-  const joke = data[randomNum];
+function renderJoke(data) {
   jokeWrapper.innerHTML = `<p class="section-titles">Daily Joke</p>
-  <p class="section-info">${joke.setup}</p>
-  <button id="joke-button" class="section-button">Reveal</button>`
-
-  const jokeButton = document.getElementById("joke-button");
-  jokeButton.addEventListener("click", (e) => {
-    e.target.remove();
-    jokeWrapper.innerHTML = `<p class="section-titles">Daily Joke</p>
-  <p class="section-info">${joke.setup}</p>
-  <p class="joke-answer">${joke.punchline}</p>`
-  });
+  <p class="section-info">${data.joke}</p>`;
 }
 
-async function getJoke(){
+async function getJoke() {
   showSpinner(jokeWrapper);
   try {
-    const response = await fetch("https://api.sampleapis.com/jokes/goodJokes");
+    const response = await fetch(
+      'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single',
+    );
 
-    if(!response.ok){
+    if (!response.ok) {
       throw response;
     }
 
     const data = await response.json();
     renderJoke(data);
-  } catch (err){
+  } catch (err) {
     showError(jokeWrapper);
   }
 }
 
 getJoke();
 
-const footballWrapper = document.getElementById("football-wrapper");
+const footballWrapper = document.getElementById('football-wrapper');
 
-function renderFootball(data){
+function renderFootball(data) {
   footballWrapper.removeChild(footballWrapper.firstChild);
-  const gamesWrapper = document.createElement("div");
-  gamesWrapper.classList.add("games-wrapper");
+  const gamesWrapper = document.createElement('div');
+  gamesWrapper.classList.add('games-wrapper');
 
   footballWrapper.append(gamesWrapper);
-  const todaysGamesP = document.createElement("p");
-  todaysGamesP.classList.add("section-titles");
-  todaysGamesP.textContent = "Next 7 Games";
+  const todaysGamesP = document.createElement('p');
+  todaysGamesP.classList.add('section-titles');
+  todaysGamesP.textContent = 'Next 7 Games';
   footballWrapper.prepend(todaysGamesP);
   data.response.forEach((fixture) => {
-    const gameContainer = document.createElement("div");
-    gameContainer.classList.add("game-container");
+    const gameContainer = document.createElement('div');
+    gameContainer.classList.add('game-container');
 
-    const homeDiv = document.createElement("div");
-    homeDiv.classList.add("team-container");
-    homeDiv.innerHTML = 
-    `<img class="team-image" src="${fixture.teams.home.logo}" alt="${fixture.teams.home.name} logo">
+    const homeDiv = document.createElement('div');
+    homeDiv.classList.add('team-container');
+    homeDiv.innerHTML = `<img class="team-image" src="${fixture.teams.home.logo}" alt="${fixture.teams.home.name} logo">
     <p class="team-name">${fixture.teams.home.name}</p>`;
     gameContainer.append(homeDiv);
 
-    const versusDiv = document.createElement("div");
-    versusDiv.classList.add("versus");
-    versusDiv.textContent = "V";
+    const versusDiv = document.createElement('div');
+    versusDiv.classList.add('versus');
+    versusDiv.textContent = 'V';
     gameContainer.append(versusDiv);
 
-    const awayDiv = document.createElement("div");
-    awayDiv.classList.add("team-container");
-    awayDiv.innerHTML = 
-    `<img class="team-image" src="${fixture.teams.away.logo}" alt="${fixture.teams.away.name} logo">
+    const awayDiv = document.createElement('div');
+    awayDiv.classList.add('team-container');
+    awayDiv.innerHTML = `<img class="team-image" src="${fixture.teams.away.logo}" alt="${fixture.teams.away.name} logo">
     <p class="team-name">${fixture.teams.away.name}</p>`;
     gameContainer.append(awayDiv);
 
     gamesWrapper.append(gameContainer);
-    });
-  }
+  });
+}
 
 const footballDate = new Date();
 const footballDateFormatted = () => {
@@ -161,23 +151,26 @@ const footballDateFormatted = () => {
   return found[0];
 };
 
-async function getFootball(){
+async function getFootball() {
   showSpinner(footballWrapper);
   try {
-    const response = await fetch(`https://v3.football.api-sports.io/fixtures?league=39&season=2022&next=7`, {
-      method: "GET",
-      headers: {
-        "x-apisports-key": "260f887bf200a22f7ac03f96833f7443"
-      }
-    });
+    const response = await fetch(
+      `https://v3.football.api-sports.io/fixtures?league=39&season=2022&next=7`,
+      {
+        method: 'GET',
+        headers: {
+          'x-apisports-key': '260f887bf200a22f7ac03f96833f7443',
+        },
+      },
+    );
 
-    if(!response.ok){
+    if (!response.ok) {
       throw response;
     }
 
     const data = await response.json();
     renderFootball(data);
-  } catch(err){
+  } catch (err) {
     showError(footballWrapper);
   }
 }
